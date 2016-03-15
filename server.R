c6a707e1da6b2209b3e14c9aca7b4f78707e1a11
patrickbarta <patrickbarta@patrickbarta.com>
2016-03-15 14:49:45 -0400

    Added files via upload
    Git repo for Developing Data Products Coursera course

---
+++ server.R
@@ -0,0 +1,54 @@
+posterior <- function(input) {
+    # compute prior odds
+    priorOdds = input$prior/(1-input$prior)
+    
+    # find likelihood ratio
+    lrt = 1.
+    if (input$humans) {
+        lrt = 2 * lrt
+    } else {
+        lrt = .5 * lrt
+    }
+    if (input$outcome) {
+        lrt = 2 * lrt
+    } else {
+        lrt = .5 * lrt
+    }
+    if (input$rct) {
+        lrt = 2 * lrt
+    } else {
+        lrt = .5 * lrt
+    }
+    if (input$enough) {
+        lrt = 2 * lrt
+    } else {
+        lrt = .5 * lrt
+    }
+    if (input$impact) {
+        lrt = 2 * lrt
+    } else {
+        lrt = .5 * lrt
+    }
+    if (input$two) {
+        lrt = 2 * lrt
+    } else {
+        lrt = .5 * lrt
+    }
+    # Bayes rule
+    postOdds = lrt*priorOdds
+    postProb = postOdds/(postOdds+1)
+    return(round(postProb, digits=5))
+}
+
+shinyServer(
+    function(input, output) {
+        output$prior <- renderText(paste("Your prior probability that study was true: ", input$prior))
+        output$humans <- renderText(paste("The study was done in humans: ", input$humans))
+        output$outcome <- renderText(paste("The outcome of the study was directly related to human health: ", input$outcome))
+        output$rct <- renderText(paste("The study was a randomized control trial: ", input$rct))
+        output$enough <- renderText(paste("There were at least hundreds of subjects: ", input$enough))
+        output$impact <- renderText(paste("The treatment had substantial impact: ", input$impact))
+        output$two <- renderText(paste("More than one group was studied: ", input$two))
+        output$post <- renderText(paste("Your posterior probability that the study is true should be: ", posterior(input)))
+    }
+)
---
+++ ui.R
@@ -0,0 +1,56 @@
+# This app can be found at https://patrickbarta.shinyapps.io/LeekScore/
+shinyUI(pageWithSidebar(
+    headerPanel("Decoding Health News: The Leek Score"),
+    sidebarPanel(
+      sliderInput("prior", 
+                    "Prior probability:", 
+                    min = 0.001,
+                    max = 0.999, 
+                    value = .5),
+      checkboxInput(inputId = "humans",
+                    label = strong("Was this a clinical study in humans?"),
+                    value = FALSE),
+      checkboxInput(inputId = "outcome",
+                    label = strong("Was the outcome directly related to health?"),
+                    value = FALSE),
+      checkboxInput(inputId = "rct",
+                    label = strong("Was this a randomized control trial?"),
+                    value = FALSE),
+      checkboxInput(inputId = "enough",
+                    label = strong("Were there at least hundreds of patients?"),
+                    value = FALSE),
+      checkboxInput(inputId = "impact",
+                    label = strong("Did treatment have major impact?"),
+                    value = FALSE),
+      checkboxInput(inputId = "two",
+                    label = strong("Do predictions hold up in at least two groups of people?"),
+                    value = FALSE)
+    ),
+    mainPanel(
+        tags$h2("Purpose"),
+        tags$p("This app implements an ", tags$a(href="http://fivethirtyeight.com/features/a-formula-for-decoding-health-news/",
+                                                  target="_blank",
+                                                "algorithm proposed by Jeff Leek"), " on the ",                                           tags$a(href="http://fivethirtyeight.com",
+                                                  target="_blank",
+                                                  "fivethirtyeight.com website"),
+                "describing how to use Bayesian reasoning to decide how credible health news is, based on some simple criteria."), 
+        tags$h2("Instructions"),
+        tags$p("In the slider to the left marked \"Prior Probability\", indicate your prior probability that you believe\
+               the findings described in the article are true. After reading the article,\
+               check the boxes on the left which are true for your article. Under Output, read your posterior probability\
+               that the findings in the article are actually true."), 
+        tags$h2("Inputs"),
+        verbatimTextOutput("prior"),
+        verbatimTextOutput("humans"),
+        verbatimTextOutput("outcome"),
+        verbatimTextOutput("rct"),
+        verbatimTextOutput("enough"),
+        verbatimTextOutput("impact"),
+        verbatimTextOutput("two"),
+        tags$h2("Output"),
+        verbatimTextOutput("post"),
+        tags$h2("Author"),
+        tags$p("Patrick Barta, patrickbarta <at> patrickbarta.com")
+        
+    )
+))
